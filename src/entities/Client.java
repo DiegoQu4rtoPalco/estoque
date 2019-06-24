df2baac1a81ee0502593e16eb6a8a6615875c7dc
Diego Santana <diego_4palco@hotmail.com>
2019-06-24 18:30:26 -0300

    Client terminado

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Client {
+	
+	private Integer quantity;
+	private Double price;
+	
+	public Client() {
+		
+	}
+	
+	public Client (Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Double subTotal () {
+		return quantity * price;
+	}
+	
 
 }
